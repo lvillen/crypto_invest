6d2c41d4e41a458059d280138dd83d1245e9fda9
Luis Villén <l.villenjaldon@gmail.com>
2021-01-19 19:48:28 +0100

    Initial commit

---
+++ cryptoinvest/views.py
@@ -0,0 +1,11 @@
+from cryptoinvest import app
+from flask import render_template, request, url_for, redirect
+import sqlite3
+
+DBFILE = app.config['DBFILE']
+
+@app.route('/')
+def movements():
+    #TODO el acceso a los movimientos
+
+    return render_template('index.html')
