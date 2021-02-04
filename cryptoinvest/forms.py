b0ae9fec177de787f67e38c08f6f42cd74b01fd9
Luis Villén <l.villenjaldon@gmail.com>
2021-02-04 15:43:04 +0100

    Primera versión funcional

--- cryptoinvest/forms.py
+++ cryptoinvest/forms.py
@@ -1,13 +1,41 @@
 from flask_wtf import FlaskForm
 from wtforms import FloatField, SubmitField, SelectField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, NumberRange
 from cryptoinvest.data.cryptos import *
 
+def actual_cryptos():
+    actual_wallet = wallet()
+    result = []
+    result.append(('1', 'EUR'))
+    for dictionary in actual_wallet:
+        if (float(dictionary['available']) > 0):
+            result.append((dictionary['id'], dictionary['name']))
+    
+    return result
+
+def available_cryptos(form, field):
+    #field.data = from_quantity.data
+    
+    available_cr = wallet()
+    currency = form.from_currency.data
+
+    for dictionary in available_cr:
+        if dictionary['id'] == int(currency):
+            if dictionary['available'] < field.data:
+                raise ValidationError('No tiene saldo suficiente.')
+
+def not_same_currency(form, field):
+    #field.data = from_quantity.data
+    from_currency = form.from_currency.data
+
+    if field.data == from_currency:
+        raise ValidationError('No puede hacer la operación, elija una moneda correcta.')
+
+
 class PurchaseForm(FlaskForm):
-    from_currency = SelectField('FromMoneda', choices=cryptos_disponibles(), validators=[DataRequired()]) 
-    from_quantity = FloatField('FromCantidad', validators=[DataRequired()])
-    #Validador personalizado /función o clase/ [DataRequired(), validarsaldo()]
-    to_currency = SelectField('ToMoneda', choices=cryptos(), validators=[DataRequired()])
+    from_currency = SelectField('FromMoneda', choices=actual_cryptos(), validators=[DataRequired()]) 
+    from_quantity = FloatField('FromCantidad', validators=[DataRequired('Introduzca un número válido, recuerde que solo puede utilizar números y que los decimales deben ir precedidos de un punto.'), available_cryptos, NumberRange(min=0.00000001, max=1000000000 ,message='Introduzca un número válido.')])
+    to_currency = SelectField('ToMoneda', choices=cryptos(), validators=[DataRequired(), not_same_currency])
     to_quantity = FloatField('ToCantidad', render_kw={'readonly': True})
     price_unit = FloatField('Precio por unidad', render_kw={'readonly': True})
 
--- cryptoinvest/views.py
+++ cryptoinvest/views.py
@@ -1,5 +1,5 @@
 from cryptoinvest import app
-from cryptoinvest.forms import PurchaseForm
+from cryptoinvest.forms import PurchaseForm, actual_cryptos
 from flask import render_template, request, url_for, redirect
 import sqlite3
 from cryptoinvest.data.movements import *
@@ -25,16 +25,28 @@ def purchase():
     to_quantity = ""
     price_unit = ""
     now = datetime.now()
-    #validate_1 = form.from_currency.validate(form) and form.from_quantity.validate(form) and form.to_currency.validate(form)
     calculate = False
 
     if request.method == 'POST':
         if form.calculate.data:
             if form.validate():
-                from_currency = get_crypto(form.from_currency.data) 
-                to_currency = get_crypto(form.to_currency.data)
-                to_quantity = round(conversion(form.from_quantity.data, from_currency, to_currency), 8)
-                price_unit = round((float(form.from_quantity.data) / to_quantity), 8)
+                try:
+                    from_currency = get_crypto(form.from_currency.data) 
+                    to_currency = get_crypto(form.to_currency.data)
+                except:
+                    #Error que vaya a la base de datos
+                    pass
+                try:
+                    to_quantity = round(conversion(form.from_quantity.data, from_currency, to_currency), 8)
+                except:
+                    #Error que vaya a la API
+                    pass
+                try:
+                    price_unit = round((float(form.from_quantity.data) / to_quantity), 8)
+                except:
+                    print("Solo para ver qué ha pasado")
+                    #Cómo controlarlo | Darle un voltio
+                
                 calculate = True
             
         """ if form.reset.data:
@@ -49,58 +61,54 @@ def purchase():
         if form.submit.data:
             if form.validate():
                 if form.from_currency.data != form.to_currency.data:
-                    consulta('INSERT INTO movements (date, time, from_currency, from_quantity, to_currency, to_quantity) VALUES (?, ?, ?, ?, ?, ?);',
-                    (
-                        now.date(),
-                        now.strftime('%H:%M:%S'),
-                        form.from_currency.data,
-                        form.from_quantity.data,
-                        form.to_currency.data,
-                        form.to_quantity.data,
-                    ))    
-
-                    return redirect(url_for('movements'))
-
-                else:
-                    print("Algo ha ido mal")
-                    return render_template('purchase.html', form=form, to_quantity=to_quantity, price_unit=price_unit)
-
-                #Tengo que volver a meter el form = PurchaseForm(request.form) Ó NO
-
-                #LLAMADA A BASE DE DATOS
-                
-                # LEER LA BASE DE DATOS
-                # CAPAR LA POSIBILIDAD DE TOCAR NADA
-                # HACER OTRO POST PARA QUE SE GRABE LA OPERACIÓN
-
-                # No limitar la vista, validación vía Python
+                    try:
+                        consulta('INSERT INTO movements (date, time, from_currency, from_quantity, to_currency, to_quantity) VALUES (?, ?, ?, ?, ?, ?);',
+                        (
+                            now.date(),
+                            now.strftime('%H:%M:%S'),
+                            form.from_currency.data,
+                            form.from_quantity.data,
+                            form.to_currency.data,
+                            form.to_quantity.data,
+                        ))    
+                        
+                        return redirect(url_for('movements'))
+                    except:
+                        #Error de base de datos
+                        #return render_template('purchase.html', form=form, to_quantity=to_quantity, price_unit=price_unit)
+                        pass
         
             else:
-                print(form.errors)
+                #En realidad quiero que me devuelva el template vacío
+                return render_template('purchase.html', form=form, to_quantity=to_quantity, price_unit=price_unit, calculate=calculate)
+
+    form.from_currency.choices = actual_cryptos()
 
     return render_template('purchase.html', form=form, to_quantity=to_quantity, price_unit=price_unit, calculate=calculate)
 
 
 @app.route('/status')
 def status():
-    print(euros_balance())
+    try:
+        the_inv = total_invested()
+    except:
+        #Error de base de datos
+        pass
+
+    try:    
+        the_bal = euros_balance()
+    except:
+        #Error de base de datos
+        pass
+
+    try:
+        the_val = actual_value()
+    except:
+        #Error de API
+        pass
+
+    the_real_value = the_inv + the_bal + the_val
+
+
     
-    return render_template('status.html', total_invested=f'{total_invested():.2f}')
-
-''' 
-    1: SALDO
-        Sumar todo lo gastado en €
-        La suma de Cantidad_to de todas los movimientos cuya
-        Moneda_to es euros - La suma de Cantidad_from de todos 
-        los movimientos cuya Moneda_from es euros
-
-    2. INVERTIDO
-        La suma de Cantidad_from cuando Moneda_from es euros
-
-    3. VALOR ACTUAL CRYPTOS
-        Bucle [qué monedas]
-            Bucle [de cada moneda, qué cantidad] > Convertir a €
-        total moneda += cantidad
-
-    return render_template('status.html', datos=status())
-'''
+    return render_template('status.html', total_invested=f'{total_invested():.2f}', actual_value=f'{the_real_value:.2f}')
